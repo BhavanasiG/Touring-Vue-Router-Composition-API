ae128689ae957f63ab2b43ce0612f1b13437e7b1
Ganesh Bhavanasi <ganesh.bhavanasi@fivium.co.uk>
2025-09-18 09:44:18 +0100

    completed L6

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,8 +1,17 @@
 <script setup>
+import { useRouter } from "vue-router";
 import { defineProps } from "vue";
 
-defineProps(["event"]);
+const props = defineProps(["event"]);
+const router = useRouter()
+
+const register = () => {
+    router.push({
+        name: 'EventDetails'
+    })
+}
 </script>
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me!</button>
 </template>
