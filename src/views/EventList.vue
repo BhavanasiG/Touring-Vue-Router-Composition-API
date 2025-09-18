82f3ecc00c6744aa484ce578e001395985589fc9
Ganesh Bhavanasi <ganesh.bhavanasi@fivium.co.uk>
2025-09-18 08:58:14 +0100

    completed L3

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,18 +1,38 @@
 <script setup>
 import EventCard from "@/components/EventCard.vue";
 import EventService from "@/services/EventService.js";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch, computed } from "vue";
+
+const props = defineProps(["page"]);
 
 const events = ref(null);
+const totalEvents = ref(0);
 
-onMounted(() => {
-  EventService.getEvents()
+const hasNextPage = computed(() => {
+  const totalPages = Math.ceil(totalEvents.value / 2);
+  return props.page < totalPages;
+});
+
+const fetchEvents = () => {
+  EventService.getEvents(2, props.page)
     .then((response) => {
       events.value = response.data;
+      totalEvents.value = response.headers["x-total-count"];
     })
     .catch((error) => {
       console.log(error);
     });
+};
+
+onMounted(() => {
+  fetchEvents();
+  watch(
+    () => props.page,
+    () => {
+      events.value = null;
+      fetchEvents();
+    }
+  );
 });
 </script>
 
@@ -20,6 +40,42 @@ onMounted(() => {
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="perv"
+        v-if="page != 1"
+        >&#60;Prev Page
+      </router-link>
+
+      <router-link
+      id="page"
+      :to="{ name: 'EventList', query: { page: 1 } }"
+      v-if="page != 1"
+      >1</router-link>
+
+      <router-link
+      id="page"
+      :to="{ name: 'EventList', query: { page: 2 } }"
+      v-if="page != 2"
+      >2</router-link>
+
+      <router-link
+      id="page"
+      :to="{ name: 'EventList', query: { page: 3 } }"
+      v-if="page != 3"
+      >3</router-link>
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >&#62;Next Page</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -28,5 +84,23 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
